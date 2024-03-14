77c4dd5838cf52b4ad1e59ae9074c9f701075ce8
vibhaarramreddy <vibharramreddy@gmail.com>
2024-03-14 16:30:00 -0400

    more requests

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -73,5 +73,81 @@ export default function QueryProcessor(query: string): string {
     }
   }
 
+  if (query.toLowerCase().includes("primes")) {
+    const numbers = query.match(/\d+/g);
+    
+    if (numbers && numbers.length >= 0) {
+
+      const primeNumbers = [];
+
+      for (const n of numbers) {
+        const number = parseInt(n);
+        let isPrime = true;
+
+        if (number <= 1) {
+          isPrime = false;
+        } else {
+          for (let i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i === 0) {
+              isPrime = false;
+              break;
+            }
+          }
+        }
+
+        if (isPrime) {
+          primeNumbers.push(number.toString());
+        }
+      }
+
+      return primeNumbers.join(', ');
+    } else {
+      return "";
+    }
+  }
+
+  if (query.toLowerCase().includes("power of")) {
+    const numbers = query.match(/\d+/g);
+
+    if (numbers && numbers.length >= 2) {
+      const x = parseInt(numbers[0]);
+      const y = parseInt(numbers[1]);
+      
+      const result = Math.pow(x,y);
+      return result.toString();
+    } else {
+      return "";
+    }
+  }
+
+  if (query.toLowerCase().includes("minus")) {
+    const numbers = query.match(/\d+/g);
+
+    if (numbers && numbers.length >= 2) {
+      const x = parseInt(numbers[0]);
+      const y = parseInt(numbers[1]);
+      
+      const result = x - y;
+      return result.toString();
+    } else {
+      return "";
+    }
+  }
+
+  if (query.toLowerCase().includes("plus") && query.toLowerCase().includes("plus")) {
+    const numbers = query.match(/\d+/g);
+
+    if (numbers && numbers.length >= 3) {
+      const x = parseInt(numbers[0]);
+      const y = parseInt(numbers[1]);
+      const z = parseInt(numbers[2]);
+      
+      const result = x + y + z;
+      return result.toString();
+    } else {
+      return "";
+    }
+  }
+
   return "";
 }
